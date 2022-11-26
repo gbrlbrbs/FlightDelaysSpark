09cd94a5aa1a3854fafef4a5d9d87fe007d72957
gbrlbrbs <gabrielbarbosamartinz_@outlook.com>
2022-11-26 20:50:34 -0300

    util to stop and clean containers

---
+++ util/clean.sh
@@ -0,0 +1,16 @@
+# clean containers
+container="$(docker ps -a | grep 'jupyterlab' | awk '{print $1}')"
+docker stop "${container}"
+docker rm "${container}"
+
+container="$(docker ps -a | grep 'spark-worker' -m 1 | awk '{print $1}')"
+while [ -n "${container}" ];
+do
+    docker stop "${container}"
+    docker rm "${container}"
+    container="$(docker ps -a | grep 'spark-worker' -m 1 | awk '{print $1}')"
+done
+
+container="$(docker ps -a | grep 'spark-master' | awk '{print $1}')"
+docker stop "${container}"
+docker rm "${container}"
